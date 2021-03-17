38e9c06ebcb08b8311e972ed744af0cabb264c07
ephanta <eva-maria.boes@informatik.hs-fulda.de>
2021-03-17 20:04:30 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const app = Vue.createApp({
+  data: function () {
+    return {
+      gender: "",
+      name: "",
+      bday: "",
+      bmonth: "",
+      byear: "",
+      color1: false,
+      color2: false,
+      color3: false,
+      color4: false,
+      comment: "",
+    };
+  },
+});
